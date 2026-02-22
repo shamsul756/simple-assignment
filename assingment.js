94af0e5c48a457e94842df2f86145541a3954260
shamsul765 <webdevlopershamsul@gmail.com>
2026-02-23 01:27:07 +0600

    collect button and element id

--- assingment.js
+++ assingment.js
@@ -3,4 +3,16 @@
 // 2.dashboard count need.
 // 3.tab button like all, interview , rejected
 // 4. interview and rejected button card inside element
+// get button
+let jobCard = document.querySelectorAll(".job-card");
+let allCard = document.getElementById('all-button');
+let interviewButton = document.getElementById('interview-button');
+let rejectedButton = document.getElementById('rejected-button');
+
+// get element 
+let totalCount = document.getElementById('total-count');
+let interviewCount = document.getElementById('interview-count');
+let rejectedCount = document.getElementById('rejected-count');
+
+// function count 
 
