dcc83987639a2eeaab88e3c63311b9f461fbc1b1
shamsul765 <webdevlopershamsul@gmail.com>
2026-02-23 01:05:04 +0600

    added parent element new class job-card

--- assingment.js
+++ assingment.js
@@ -1,1 +1,6 @@
-console.log('my love ');
+// first work go to js file then i catch all element .most importantly which are my need .
+// 1.all job card need.
+// 2.dashboard count need.
+// 3.tab button like all, interview , rejected
+// 4. interview and rejected button card inside element
+
